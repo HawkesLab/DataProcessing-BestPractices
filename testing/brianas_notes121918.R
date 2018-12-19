749023e30b95901460c10395dd67d8fa45a04f80
“Briana <brkawhit@indiana.edu>
2018-12-19 14:42:30 -0500

    notes for briana on today's lesson

---
+++ testing/brianas_notes121918.R
@@ -0,0 +1,7 @@
+#git is a software
+#GItHub is a web based host for git related items
+#they are not the same
+
+#download vs. clone - not the same, cloning allows the git software to keep an eye on it
+
+#Rproj files are useful for sharing
